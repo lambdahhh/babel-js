45c99a0d3b3cbdcd0e0da45833e11161ef434cc4
pwooolf <lambdahhh@gmail.com>
2022-01-17 00:49:42 +0300

    167 simple webpakc conf

--- build/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-var App = function App() {
-  return /*#__PURE__*/React.createElement("p", null, "Hello");
-};
-
-ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('root'));
