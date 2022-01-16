fd14fd0c7fd8a3cae1ea8835ebe3e2e31c5fc5f6
pwooolf <lambdahhh@gmail.com>
2022-01-16 23:59:35 +0300

    164 react jsx

--- build/main.js
+++ build/main.js
@@ -1,45 +1,8 @@
-import "regenerator-runtime/runtime.js";
-import "core-js/modules/es.array.flat.js";
-import "core-js/modules/es.array.unscopables.flat.js";
-import "core-js/modules/es.object.to-string.js";
-import "core-js/modules/es.promise.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+var App = function App() {
+  return /*#__PURE__*/React.createElement("p", null, "Hello");
+};
 
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-var App = /*#__PURE__*/_createClass(function App() {
-  _classCallCheck(this, App);
-
-  _defineProperty(this, "run", /*#__PURE__*/_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-    var name,
-        _args = arguments;
-    return regeneratorRuntime.wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            name = _args.length > 0 && _args[0] !== undefined ? _args[0] : 'World';
-            console.log("Hello, ".concat(name));
-            console.log([1, 2, [2, 3]].flat());
-
-          case 3:
-          case "end":
-            return _context.stop();
-        }
-      }
-    }, _callee);
-  })));
-});
-
-var app = new App();
-app.run().then(function () {
-  return console.log('here');
-});
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('root'));
--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
-class App {
-    run = async (name = 'World') => {
-        console.log(`Hello, ${name}`);
-        console.log([1,2,[2,3]].flat());
-    }
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const app = new App();
-app.run().then(() => console.log('here'));
+const App = () => <p>Hello</p>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
