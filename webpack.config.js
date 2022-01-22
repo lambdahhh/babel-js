e70a52b7ba45a3a8612a3720df19c905ce1513a9
pwooolf <lambdahhh@gmail.com>
2022-01-22 23:30:10 +0300

    180 webpack prod config

--- webpack.config.js
+++ webpack.config.js
@@ -3,68 +3,93 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const path = require("path");
 
-module.exports = {
-    mode: "development",
-    output: {
-        path: path.resolve("dist"),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            // для dev 'style-loader', для прода MiniCssExtractPlugin.loader - на деве не перезагружает
-            {
-                test: /\.(s[ca]ss)$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            // load images
-            {
-                test: /\.(png|jpg|jpeg|gif|ico)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'images',
-                            name: '[name]-[sha1:chunkhash:7].[ext]'
-                        }
-                    }
-                ]
-            },
-            // load fonts
-            {
-                test: /\.(ttf|otf|eot|woff|woff2)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'fonts',
-                            name: '[name].[ext]'
-                        }
-                    }
-                ]
-            }
+module.exports = (env) => {
+
+    const {mode = 'development'} = env;
+
+    const isProd = mode === 'production';
+    const isDev = mode === 'development';
+
+    const getStyleLoaders = () => {
+        return [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader'
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Hello React World',
-            buildTime: new Date().toString(),
-            template: 'public/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'main-[chunkhash:8].css'
-        })
-    ],
-    "devServer":{
-        open: true,
-        hot: true
+    }
+
+    const getPlugins = () => {
+        const plugins = [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                title: 'Hello React World',
+                buildTime: new Date().toString(),
+                template: 'public/index.html'
+            }),
+
+        ];
+        if (isProd) {
+            plugins.push(
+                new MiniCssExtractPlugin({
+                    filename: 'main-[chunkhash:8].css'
+                })
+            );
+        }
+
+        return plugins;
+    }
+
+    return {
+        mode: isProd ? 'production' : isDev && 'development',
+        output: {
+            path: path.resolve("dist"),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                },
+                {
+                    test: /\.(s[ca]ss)$/,
+                    use: [...getStyleLoaders(), 'sass-loader']
+                },
+                {
+                    test: /\.css$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader']
+                },
+                // load images
+                {
+                    test: /\.(png|jpg|jpeg|gif|ico)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'images',
+                                name: '[name]-[sha1:chunkhash:7].[ext]'
+                            }
+                        }
+                    ]
+                },
+                // load fonts
+                {
+                    test: /\.(ttf|otf|eot|woff|woff2)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'fonts',
+                                name: '[name].[ext]'
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: getPlugins(),
+        "devServer": {
+            open: true,
+            hot: true
+        }
     }
 };
